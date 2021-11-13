ef70bbd9507c9797fb8e2a0430d2056a0195e12e
XNORF <78629634+XNORF@users.noreply.github.com>
2021-11-14 00:52:48 +0800

    remove billing

--- Adopter/accountadopter.php
+++ Adopter/accountadopter.php
@@ -114,7 +114,7 @@ if (isset($_GET['update'])) {
                         <ul>
                             <li><a href="adopt.php">Adopt</a></li>
                             <li><a href="donation/donation.php">Donate</a></li>
-                            
+
                         </ul>
                     </li>
 
@@ -150,10 +150,9 @@ if (isset($_GET['update'])) {
                         <nav class="side-menu">
                             <ul class="nav">
                                 <li class="active"><a href="#"><span class="fa fa-user"></span> Profile</a></li>
-                                <li><a href="billingadopter.php"><span class="fa fa-credit-card"></span> Billing</a></li>
                                 <li><a href="adoptionhistoryadopter.php"><span class="fa fa-envelope"></span> Adoption History</a></li>
                                 <li><a href="changepassadopter.php"><span class="fa fa-th"></span> Change Password</a></li>
-                                
+
                             </ul>
                         </nav>
                     </div>
