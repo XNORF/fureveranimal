5f4741cf99e5d21f5da3f2314f4e122ea3101089
dinifar <dinifarizahyazid@gmail.com>
2021-11-14 22:23:34 +0800

    adopter history table (transaction&appointment)

--- Adopter/adoptionhistoryadopter.php
+++ Adopter/adoptionhistoryadopter.php
@@ -140,244 +140,196 @@ if (!$GLOBALS['con']) {
 								<div class="form-group">
 								</div>
 
-								<div class="container-xl">
-									<div class="table-responsive">
-										<div class="table-wrapper">
-											<div class="table-title">
-												<div class="row">
-													<div class="col-sm-6">
-														<h2>Adoption <b>History</b></h2>
-													</div>
-													<div class="col-sm-6">
-														<!--<a href="#addAdoptionModal" class="btn btn-success" data-toggle="modal"><i class="material-icons">&#xE147;</i> <span>Add New Employee</span></a>-->
-														<a href="#deleteAdoptionModal" class="btn btn-danger" data-toggle="modal"><i class="material-icons">&#xE15C;</i> <span>Delete</span></a>
-													</div>
-												</div>
-											</div><br>
-											<table class="table table-striped table-hover">
-												<thead>
-													<tr>
-														<th>
-															<span class="custom-checkbox">
-																<input type="checkbox" id="selectAll">
-																<label for="selectAll"></label>
-															</span>
-														</th>
-														<th>Name</th>
-														<th>Email</th>
-														<th>Address</th>
-														<th>Phone</th>
-														<th>Animal Adopted</th>
-														<th>Date Adopted</th>
-														<th>Amount</th>
-														<th>Actions</th>
-													</tr>
-												</thead>
-												<tbody>
-													<tr>
-														<td>
-															<span class="custom-checkbox">
-																<input type="checkbox" id="checkbox1" name="options[]" value="1">
-																<label for="checkbox1"></label>
-															</span>
-														</td>
-														<td>Thomas Hardy</td>
-														<td>thomashardy@mail.com</td>
-														<td>89 Chiaroscuro Rd, Portland, USA</td>
-														<td>(171) 555-2222</td>
-														<td> Lily </td>
-														<td> 5 September 2021</td>
-														<td> RM 200 </td>
-														<td>
-															<!--<a href="#editAdopterModal" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>-->
-															<a href="#deleteAdopterModal" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
-														</td>
-													</tr>
-													<tr>
-														<td>
-															<span class="custom-checkbox">
-																<input type="checkbox" id="checkbox2" name="options[]" value="1">
-																<label for="checkbox2"></label>
-															</span>
-														</td>
-														<td>Dominique Perrier</td>
-														<td>dominiqueperrier@mail.com</td>
-														<td>Obere Str. 57, Berlin, Germany</td>
-														<td>(313) 555-5735</td>
-														<td> Nyx </td>
-														<td> 5 September 2021</td>
-														<td> RM 200 </td>
-														<td>
-															<!--<a href="#editAdopterModal" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>-->
-															<a href="#deleteAdopterModal" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
-														</td>
-													</tr>
-													<tr>
-														<td>
-															<span class="custom-checkbox">
-																<input type="checkbox" id="checkbox3" name="options[]" value="1">
-																<label for="checkbox3"></label>
-															</span>
-														</td>
-														<td>Maria Anders</td>
-														<td>mariaanders@mail.com</td>
-														<td>25, rue Lauriston, Paris, France</td>
-														<td>(503) 555-9931</td>
-														<td> King </td>
-														<td> 5 September 2021</td>
-														<td> RM 200 </td>
-														<td>
-															<!--<a href="#editAdopterModal" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>-->
-															<a href="#deleteAdopterModal" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
-														</td>
-													</tr>
-													<tr>
-														<td>
-															<span class="custom-checkbox">
-																<input type="checkbox" id="checkbox4" name="options[]" value="1">
-																<label for="checkbox4"></label>
-															</span>
-														</td>
-														<td>Fran Wilson</td>
-														<td>franwilson@mail.com</td>
-														<td>C/ Araquil, 67, Madrid, Spain</td>
-														<td>(204) 619-5731</td>
-														<td> Killa </td>
-														<td> 5 September 2021</td>
-														<td> RM 200 </td>
-														<td>
-															<!--<a href="#editAdopterModal" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>-->
-															<a href="#deleteAdopterModal" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
-														</td>
-													</tr>
-													<tr>
-														<td>
-															<span class="custom-checkbox">
-																<input type="checkbox" id="checkbox5" name="options[]" value="1">
-																<label for="checkbox5"></label>
-															</span>
-														</td>
-														<td>Martin Blank</td>
-														<td>martinblank@mail.com</td>
-														<td>Via Monte Bianco 34, Turin, Italy</td>
-														<td>(480) 631-2097</td>
-														<td> Richard </td>
-														<td> 5 September 2021</td>
-														<td> RM 200 </td>
-														<td>
-															<!--<a href="#editAdopterModal" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>-->
-															<a href="#deleteAdopterModal" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
-														</td>
-													</tr>
-												</tbody>
-											</table>
-											<div class="clearfix">
-												<div class="hint-text">Showing <b>5</b> out of <b>25</b> entries</div><br>
-												<ul class="pagination">
-													<li class="page-item"><a href="#" class="page-link">Previous</a></li>
-													<li class="page-item"><a href="#" class="page-link">1</a></li>
-													<li class="page-item"><a href="#" class="page-link">2</a></li>
-													<li class="page-item active"><a href="#" class="page-link">3</a></li>
-													<li class="page-item"><a href="#" class="page-link">4</a></li>
-													<li class="page-item"><a href="#" class="page-link">5</a></li>
-													<li class="page-item"><a href="#" class="page-link">Next</a></li>
-												</ul>
-											</div>
-										</div>
-									</div>
-								</div>
-								<!-- Edit Modal HTML -->
-								<div id="addEmployeeModal" class="modal fade">
-									<div class="modal-dialog">
-										<div class="modal-content">
-											<form>
-												<div class="modal-header">
-													<h4 class="modal-title">Add</h4>
-													<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-												</div>
-												<div class="modal-body">
-													<div class="form-group">
-														<label>Name</label>
-														<input type="text" class="form-control" required>
-													</div>
-													<div class="form-group">
-														<label>Email</label>
-														<input type="email" class="form-control" required>
-													</div>
-													<div class="form-group">
-														<label>Address</label>
-														<textarea class="form-control" required></textarea>
-													</div>
-													<div class="form-group">
-														<label>Phone</label>
-														<input type="text" class="form-control" required>
-													</div>
-												</div>
-												<div class="modal-footer">
-													<input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel">
-													<input type="submit" class="btn btn-success" value="Add">
-												</div>
-											</form>
-										</div>
-									</div>
-								</div>
-								<!-- Edit Modal HTML -->
-								<div id="editEmployeeModal" class="modal fade">
-									<div class="modal-dialog">
-										<div class="modal-content">
-											<form>
-												<div class="modal-header">
-													<h4 class="modal-title">Edit</h4>
-													<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-												</div>
-												<div class="modal-body">
-													<div class="form-group">
-														<label>Name</label>
-														<input type="text" class="form-control" required>
-													</div>
-													<div class="form-group">
-														<label>Email</label>
-														<input type="email" class="form-control" required>
-													</div>
-													<div class="form-group">
-														<label>Address</label>
-														<textarea class="form-control" required></textarea>
-													</div>
-													<div class="form-group">
-														<label>Phone</label>
-														<input type="text" class="form-control" required>
-													</div>
-												</div>
-												<div class="modal-footer">
-													<input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel">
-													<input type="submit" class="btn btn-info" value="Save">
-												</div>
-											</form>
-										</div>
-									</div>
-								</div>
-								<!-- Delete Modal HTML -->
-								<div id="deleteEmployeeModal" class="modal fade">
-									<div class="modal-dialog">
-										<div class="modal-content">
-											<form>
-												<div class="modal-header">
-													<h4 class="modal-title">Delete</h4>
-													<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-												</div>
-												<div class="modal-body">
-													<p>Are you sure you want to delete these Records?</p>
-													<p class="text-warning"><small>This action cannot be undone.</small></p>
-												</div>
-												<div class="modal-footer">
-													<input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel">
-													<input type="submit" class="btn btn-danger" value="Delete">
-												</div>
-											</form>
-										</div>
-									</div>
-								</div>
-					</div>
+								<!-- partial -->
+
+      <div class="main-panel">
+        <div class="content-wrapper">
+          <div class="container">
+            <div class="row">
+              <h3 class="page-title col-md-1">
+                <center>ADOPTION</center>
+              </h3>
+             
+
+          <div class="container mt-2">
+            <div class="row">
+              <div class="col-md-12">
+                <div class="">
+                  <div class="">
+                    <?php
+
+                    $db = mysqli_select_db($GLOBALS['con'], 'fureveranimalshelter');
+
+					$query = "SELECT * FROM transaction";
+					
+                    $query_run = mysqli_query($GLOBALS['con'], $query);
+
+                    ?>
+
+
+                    <table class="table" style="background-color:#F0C6F2">
+                      <div class="table-responsive">
+                        <table class="table">
+                          <tbody>
+                            <!-- On tables -->
+
+                            <table id="appointmentTable" class="table table-light table-hover table-striped table-md">
+                              <thead>
+                                <tr>
+
+                                  <th scope="col" class="th-sm">ADOPTER</th>
+                                  <th scope="col" class="th-sm">TYPE</th>
+                                  <th scope="col" class="th-sm">DETAILS</th>
+                                  <th scope="col" class="th-sm">AMOUNT</th>
+                                  <th scope="col" class="th-sm">DATED</th>
+                              
+                                </tr>
+                              </thead>
+                              <tbody>
+
+                              <?php
+
+
+                                if (mysqli_num_rows($query_run) > 0) {
+
+                                  foreach ($query_run as $row) {
+                                ?>
+                                    <tr class="text-black">
+
+                                      
+                                        <h1 visibility: hidden><?php echo $row['id']; ?></h1>
+                                       
+                                      <td> <?php echo $row['adopter']; ?> </td>
+                                      <td> <?php echo $row['type']; ?> </td>
+                                      <td> <?php echo $row['details']; ?> </td>
+                                      <td> <?php echo $row['amount']; ?> </td>
+                                      <td> <?php echo $row['dated']; ?> </td>
+                                     
+                                    </tr>
+
+                                  <?php
+                                  }
+                                } else {
+                                  ?>
+
+                                  <tr>
+
+                                    <td>No Record Available</td>
+
+                                  </tr>
+
+                                <?php
+                                }
+                                ?>
+
+                              </tbody>
+                            </table>
+
+						<fieldset class="fieldset">
+								<div class="form-group">
+								</div>	
+								
+								<br><br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+								<!-- partial -->
+
+      <div class="main-panel">
+        <div class="content-wrapper">
+          <div class="container">
+            <div class="row">
+              <h3 class="page-title col-md-1">
+                <center>APPOINTMENT</center>
+              </h3>
+             
+
+          <div class="container mt-2">
+            <div class="row">
+              <div class="col-md-12">
+                <div class="">
+                  <div class="">
+                    <?php
+
+                    $db = mysqli_select_db($GLOBALS['con'], 'fureveranimalshelter');
+
+					$query = "SELECT * FROM appointment";
+                    $query_run = mysqli_query($GLOBALS['con'], $query);
+
+				
+
+                    ?>
+
+
+
+                    <table class="table" style="background-color:#F0C6F2">
+                      <div class="table-responsive">
+                        <table class="table">
+                          <tbody>
+                            <!-- On tables -->
+
+                            <table id="appointmentTable" class="table table-light table-hover table-striped table-md">
+                              <thead>
+                                <tr>
+
+                                  <th scope="col" class="th-sm">ADOPTER</th>
+                                  <th scope="col" class="th-sm">PET</th>
+                                  <th scope="col" class="th-sm">DATE</th>
+                                  <th scope="col" class="th-sm">TIME</th>
+                                  <th scope="col" class="th-sm">STATUS</th>
+                                  
+                                </tr>
+                              </thead>
+                              <tbody>
+
+                              <?php
+
+
+                                if (mysqli_num_rows($query_run) > 0) {
+
+                                  foreach ($query_run as $row) {
+                                ?>
+                                    <tr class="text-black">
+
+                                      
+                                        <h1 visibility: hidden><?php echo $row['id']; ?></h1>
+                                       
+                                      <td> <?php echo $row['adopter']; ?> </td>
+                                      <td> <?php echo $row['pet']; ?> </td>
+                                      <td> <?php echo $row['date']; ?> </td>
+                                      <td> <?php echo $row['time']; ?> </td>
+                                      <td> <?php
+                                            if ($row['status'] == 0) {
+                                              echo "UPCOMING APPOINTMENT";
+                                            } else if ($row['status'] == 1) {
+                                              echo "DONE";
+                                            } 
+                                            ?> </td>
+                                      <!--<td> <?php //echo $row['story']; 
+                                                ?> </td>-->
+                                    
+                                    </tr>
+
+                                  <?php
+                                  }
+                                } else {
+                                  ?>
+
+                                  <tr>
+
+                                    <td>No Record Available</td>
+
+                                  </tr>
+
+                                <?php
+                                }
+                                ?>
+
+                              </tbody>
+                            </table>
+
 					</fieldset>
 					<hr>
 
